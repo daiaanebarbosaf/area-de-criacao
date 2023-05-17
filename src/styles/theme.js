4f748101768a720cba9f037a6f2e82963930ecc3
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-05-17 20:08:24 -0300

    Create theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    WHITE: "#FBF6FF",
+    ORANGE_100: "#FFBD59",
+    ORANGE_200: "#EF7B02",
+    ORANGE_300: "#EA5B0C",
+
+    GRAY_100: "#575756",
+    GRAY_300: "#4A4A49",
+
+    BLACK: "#000000",
+    PURBLE: "#290742",
+
+    RED: "#FF002E"
+  }
+}
